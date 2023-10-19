10c2bbc9f659cdfa9223166d657dff4a0ff58f52
keurim06 <130616662+keurim06@users.noreply.github.com>
2023-10-18 21:13:16 -0300

    Add files via upload

---
+++ -robotron-2000-projeto_inicial/js/main.js
@@ -0,0 +1,12 @@
+const robotron = document.querySelector(#robotron)
+
+robotron.addEventListener("click", () => {
+    console.log("clique no robô.")
+})
+
+function dizOi(nome) {
+    console.log("oi" + nome)
+    console.log("bem-vindo ao robotron 2000")
+    
+}
+dizOi("Pedro")
